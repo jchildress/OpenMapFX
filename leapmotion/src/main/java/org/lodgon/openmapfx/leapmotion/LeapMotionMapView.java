d68f0c7871d73b492d7a98281523b87eea44680a
Johan Vos <johan@lodgon.com>
2014-06-03 20:55:15 +0200

    copyright

--- leapmotion/src/main/java/org/lodgon/openmapfx/leapmotion/LeapMotionMapView.java
+++ leapmotion/src/main/java/org/lodgon/openmapfx/leapmotion/LeapMotionMapView.java
@@ -1,5 +1,6 @@
 /**
- * Copyright (c) 2014, Johan Vos, LodgON All rights reserved.
+ * Copyright (c) 2014, OpenMapFX
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met: *
