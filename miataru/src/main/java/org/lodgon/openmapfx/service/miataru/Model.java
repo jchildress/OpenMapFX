6d925c4f49c47143a2af7130ef54b2894ab84788
Joeri Sykora <joeri@lodgon.com>
2014-06-18 11:57:40 +0200

    server location from settings

--- miataru/src/main/java/org/lodgon/openmapfx/service/miataru/Model.java
+++ miataru/src/main/java/org/lodgon/openmapfx/service/miataru/Model.java
@@ -1,35 +1,31 @@
 /**
- * Copyright (c) 2014, OpenMapFX
- * All rights reserved.
+ * Copyright (c) 2014, OpenMapFX All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *     * Neither the name of LodgON, the website lodgon.com, nor the
- * names of its contributors may be used to endorse or promote products
+ * modification, are permitted provided that the following conditions are met: *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. * Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. * Neither the name of LodgON, the website lodgon.com,
+ * nor the names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL LODGON BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL LODGON BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.lodgon.openmapfx.service.miataru;
 
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
@@ -40,16 +36,21 @@ import javafx.collections.ObservableList;
  * @author johan
  */
 public class Model {
-    
+
     private final static Model instance = new Model();
 
-    private final BooleanProperty trackProperty = new SimpleBooleanProperty(true);
+    private final BooleanProperty trackingProperty = new SimpleBooleanProperty(true);
     private final BooleanProperty historyEnabledProperty = new SimpleBooleanProperty(false);
-    private final StringProperty deviceNameProperty = new SimpleStringProperty();
+    private final StringProperty deviceNameProperty = new SimpleStringProperty("Demo Device");
+//    private final StringProperty serverProperty = new SimpleStringProperty("http://192.168.1.6:8080/miataru/v1");
+    private final StringProperty serverLocationProperty = new SimpleStringProperty("service.miataru.com");
+//    private final StringProperty serverProperty = new SimpleStringProperty("http://localhost:8080/miataru/v1");
+//    private final StringProperty serverProperty = new SimpleStringProperty("http://lodgon.dyndns.org:9999/miataru/v1");
     private final ObservableList<Device> trackingDevices = FXCollections.observableArrayList();
 
-    private Model() {}
-    
+    private Model() {
+    }
+
     public static Model getInstance() {
         return instance;
     }
@@ -62,12 +63,52 @@ public class Model {
         return deviceNameProperty;
     }
 
-    public BooleanProperty trackProperty() {
-        return trackProperty;
+    public String getDeviceName() {
+        return deviceNameProperty.get();
+    }
+
+    public StringProperty serverLocationProperty() {
+        return serverLocationProperty;
+    }
+
+    public String getServerLocation() {
+        return serverLocationProperty.get();
+    }
+
+    public String getServerServiceLocation() {
+        String serverServiceLocation = "";
+
+        String serverLocation = getServerLocation();
+        if (serverLocation.startsWith("http")) {
+            serverServiceLocation = serverLocation;
+        } else {
+            serverServiceLocation = "http://" + serverLocation;
+        }
+
+        if (!serverServiceLocation.endsWith("/")) {
+            serverServiceLocation += "/";
+        }
+        if (!serverServiceLocation.endsWith("v1/")) {
+            serverServiceLocation += "v1/";
+        }
+
+        return serverServiceLocation;
+    }
+
+    public BooleanProperty trackingProperty() {
+        return trackingProperty;
+    }
+
+    public boolean isTracking() {
+        return trackingProperty.get();
     }
 
     public BooleanProperty historyEnabledProperty() {
         return historyEnabledProperty;
+    }
+
+    public boolean isHistoryEnabled() {
+        return historyEnabledProperty.get();
     }
 
 }
